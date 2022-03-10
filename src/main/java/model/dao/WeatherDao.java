4175995c53553c397229d34ad805e33d9bcff624
Łukasz Lewandowski <lukastonie@gmail.com>
2022-03-10 14:05:37 +0100

    weatherDao(save, findByID methods)

---
+++ src/main/java/model/dao/WeatherDao.java
@@ -0,0 +1,47 @@
+package model.dao;
+
+import model.Weather;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.connection.HibernateUtil;
+
+public class WeatherDao {
+
+    public void save(Weather weather) {
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.saveOrUpdate(weather);
+
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
+    }
+    public void delete(Weather weather) {
+    }
+
+
+
+    public Weather findById(Integer id) {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            Weather weather = session.get(Weather.class, id);
+
+            transaction.commit();
+
+            return weather;
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
+        return null;
+    }
+}
---
+++ src/test/java/model/dao/WeatherDaoTest.java
@@ -0,0 +1,67 @@
+package model.dao;
+
+import model.Location;
+import model.Weather;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherDaoTest {
+    WeatherDao weatherDao = new WeatherDao();
+    LocationDao locationDao = new LocationDao();
+    Weather weather;
+    Weather weather1;
+
+    @BeforeEach
+    void setUp(){
+        weather = Weather.Builder()
+                .withDate(LocalDate.now())
+                .withHumidity(11)
+                .withPressure(111)
+                .withTemperature(4.0)
+                .withWindDegrees(1)
+                .withWindDirection("S")
+                .withWindSpeed(33)
+                .withLocation(new Location())
+                .build();
+
+        weather1 = Weather.Builder()
+                .withDate(LocalDate.now())
+                .withHumidity(22)
+                .withPressure(567)
+                .withTemperature(4.0)
+                .withWindDegrees(1)
+                .withWindDirection("S")
+                .withWindSpeed(33)
+                .withLocation(new Location())
+                .build();
+        weatherDao.save(weather);
+        weatherDao.save(weather1);
+    }
+
+    @Test
+    void shouldSaveWeather() {
+        Weather toSave = Weather.Builder()
+                .withDate(LocalDate.now())
+                .withHumidity(33)
+                .withPressure(587)
+                .withTemperature(4.0)
+                .withWindDegrees(1)
+                .withWindDirection("S")
+                .withWindSpeed(33)
+                .withLocation(new Location(1,22.22,33.33,"KRK", "PL", "Mazowieckie", null))
+                .build();
+
+        weatherDao.save(toSave);
+
+        Weather expected = weatherDao.findById(3);
+
+        assertThat(expected).isEqualTo(toSave);
+
+
+    }
+}
